31943f8db0fd04dfd889311c5098ae7ee1b1947c
Abhishek Pratap Singh <singhabhishek2750@gmail.com>
2026-09-13 21:40:16 +0530

    Add Main class with student display functionality

---
+++ Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Student student = new Student(
+            "Abhishek",
+            "abhishek@gmail.com",
+            "Java Programming"
+        );
+
+        System.out.println("SMART LEARNING MANAGEMENT SYSTEM");
+        System.out.println("--------------------------------");
+        student.displayStudent();
+    }
+}
